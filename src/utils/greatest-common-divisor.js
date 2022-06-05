ee5957164ffc6dfd84bffd57eb40ebf31d1cdd6a
YSidorko <yury.sidorko@gmail.com>
2022-06-05 18:00:37 +0200

    - get Greatest Common Divisor (GCD) - more than 2 numbers

---
+++ src/utils/greatest-common-divisor.js
@@ -0,0 +1,33 @@
+// get greatest common divisor (GCD)
+function getGCD(array) {
+  if (!Array.isArray(array) || array.length <= 0) return null;
+
+  let min = Math.min(...array);
+  if (!min || min <= 0) return null;
+
+  const isAllNatural = array.every((value) => value % 1 === 0);
+  if (!isAllNatural) return null;
+
+  let _array = array;
+
+  while (_array.length) {
+    let _min = min;
+    _array = _array.map((value) => {
+      const mod = value % min;
+      if ((mod > 0) && (_min > mod)) _min = mod;
+      return mod;
+    }).filter((value) => value);
+
+    // update minimum
+    if (_array.length) {
+      _array.push(min);
+      min = _min;
+    }
+  }
+
+  return min;
+}
+
+module.exports = {
+  getGCD
+}
---
+++ src/utils/greatest-common-divisor.test.js
@@ -0,0 +1,46 @@
+const { getGCD } = require('./greatest-common-divisor');
+
+test(`getNOD is function`, () => {
+  expect(typeof getGCD).toBe('function');
+});
+
+test('NOD 96, 132, 144', () => {
+  expect(getGCD([96, 132, 144])).toBe(12);
+})
+
+test('NOD 624960, 49104, 66960', () => {
+  expect(getGCD([624960, 49104, 66960])).toBe(4464);
+})
+
+test('NOD 126 435 279', () => {
+  expect(getGCD([126, 435, 279])).toBe(3);
+})
+
+test('NOD -1, 5', () => {
+  expect(getGCD([-1, 5])).toBe(null);
+})
+
+test('NOD []', () => {
+  expect(getGCD([])).toBe(null);
+})
+
+test('NOD "[]"', () => {
+  expect(getGCD('[]')).toBe(null);
+})
+
+test('NOD 400, 100, 20, 80', () => {
+  expect(getGCD([400, 100, 20, 80])).toBe(20);
+})
+
+test('NOD 24654, 25473, 954', () => {
+  expect(getGCD([24654, 25473, 954])).toBe(3);
+})
+
+test('Invalid input', () => {
+  expect(getGCD([null])).toBe(null);
+  expect(getGCD([null, 5, 10, null])).toBe(null);
+  expect(getGCD([-NaN, 5, NaN])).toBe(null);
+  expect(getGCD([5, 1, 's'])).toBe(null);
+  expect(getGCD([5.1, 1, 3])).toBe(null);
+  expect(getGCD(3)).toBe(null);
+})
